9e6e35c8e18c68e6de952de9e2ee640cebc13cc0
Ephrame Amanu <ephremeamanu@gmail.com>
2025-05-20 23:35:04 +0300

    metta pattern matching function

--- task.py
+++ task.py
@@ -1,4 +1,22 @@
+import os
+
+
 from hyperon import *
 
 metta=MeTTa()
 
+with open('data.metta', 'r') as file:
+    content = file.read()
+    metta.run(content)
+# test=metta.run('!(match &self ($x $y $z $w) ($x $y $z $w))')
+
+metta.run('''(= (pattern $par)
+(let* (
+        ($val (match &self ($x $y $z $w) ($x $y $z $w)))
+        ($ingredient (index-atom $val 1))
+)
+(if (== () (subtraction-atom $ingredient $par)) $val (empty))
+))''')
+
+check = metta.run('!(pattern (Banana Strawberries Yogurt Honey))')
+print(check)
